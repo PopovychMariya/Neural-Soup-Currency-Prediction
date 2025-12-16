d13e9271572d1331f1059712f02711cb383c1d1b
Hryb <gribvalerij6@gmail.com>
2025-12-16 10:16:52 +0200

    removed missing currencies

--- constants.py
+++ constants.py
@@ -2,7 +2,5 @@ CHECKPOINT_PATH = "checkpoints/checkpoint.pth"
 CURRENCY_PAIR_TO_TICKER = {
     "USD-UAH": "USDUAH=X",  # 1 USD = ? UAH
     "EUR-UAH": "EURUAH=X",  # 1 EUR = ? UAH
-    "CNY-UAH": "CNYUAH=X",  # 1 CNY = ? UAH
     "GBP-UAH": "GBPUAH=X",  # 1 GBP = ? UAH
-    "PLN-UAH": "PLNUAH=X",  # 1 PLN = ? UAH
 }
